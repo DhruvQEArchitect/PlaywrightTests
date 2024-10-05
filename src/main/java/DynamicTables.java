b1ffa3b985f4481afaa3aa36a4b13319a8f277ca
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2024-10-05 11:16:28 -0400

    updating logic for webtable handling advanced level

---
+++ src/main/java/DynamicTables.java
@@ -0,0 +1,41 @@
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
+
+import java.nio.file.Paths;
+
+public class DynamicTables {
+    static Page page;
+
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            BrowserContext browserContext = browser.newContext();
+            page = browserContext.newPage();
+            page.navigate("https://primeng.org/");
+            System.out.println(page.title());
+            if (page.locator("h5.m-0").textContent().equals("Customers")) {
+                System.out.println("Verified table header successfully");
+            }
+            selectCustomer("Meaghan Garufi");
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("dynamicTable.png")));
+        }
+    }
+
+    static void selectCustomer(String customerName) {
+        Locator getUpperLimitOfPage = page.locator("//span[@class='p-paginator-current']");
+        String limit = getUpperLimitOfPage.textContent();
+        String[] temp = limit.split(" ");
+        System.out.println(temp[temp.length - 1]);
+        int j = 0, i = Integer.parseInt(temp[temp.length - 1]);
+        while (j < i) {
+            if (page.locator("//*[text()=' " + customerName + " ']").isVisible()) {
+                page.locator("//*[text()=' " + customerName + " ']/ancestor::tr/td[1]").click();
+                break;
+            } else if (page.locator("//button[@class='p-ripple p-element p-paginator-next p-paginator-element p-link']").isEnabled()
+                    && page.locator("//button[@class='p-ripple p-element p-paginator-next p-paginator-element p-link']").isVisible()) {
+                page.locator("//button[@class='p-ripple p-element p-paginator-next p-paginator-element p-link']").click();
+                j++;
+            }
+        }
+    }
+}
---
+++ src/main/java/FillForm.java
@@ -0,0 +1,39 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
+import com.microsoft.playwright.options.AriaRole;
+
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FillForm {
+
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            BrowserContext browserContext = browser.newContext();
+            Page page = browserContext.newPage();
+            page.navigate("https://www.orangehrm.com/en/30-day-free-trial");
+            page.getByPlaceholder("Pick a username for your OrangeHRM trial.").fill("test");
+            System.out.println(page.locator("span.description").textContent());
+            PlaywrightAssertions.assertThat(page.locator("span.description")).isVisible();
+            page.getByPlaceholder("Your Full Name*").fill("test");
+            page.getByPlaceholder("Business Email*").fill("test");
+            page.getByPlaceholder("Phone Number*").fill("test");
+            List<String> getAllCountries = page.locator("select#Form_getForm_Country").allTextContents();
+            getAllCountries.forEach(System.out::println);
+            page.getByText("Get Your Free Trial").click();
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("form.png")));
+
+            Browser browser2 = playwright.firefox().launch();
+            BrowserContext browserContext2 = browser2.newContext();
+            Page page2 = browserContext2.newPage();
+            page2.navigate("https://www.bigbasket.com");
+            List<String> aboutList = page2.locator("div.container div[class='w-1/3 pr-4'] a").allTextContents();
+            aboutList.forEach(System.out::println);
+            page2.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("bigbasket.png")));
+        }
+    }
+}
