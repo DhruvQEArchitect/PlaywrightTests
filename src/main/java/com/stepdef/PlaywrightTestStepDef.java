88e9b66c5bb3614b40a8d0cd16d5e349ffcfb57b
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2025-03-02 20:48:56 -0500

    playwright and cucumber integration

--- src/main/java/com/stepdef/PlaywrightTestStepDef.java
+++ src/main/java/com/stepdef/PlaywrightTestStepDef.java
@@ -4,6 +4,7 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -24,13 +25,25 @@ public class PlaywrightTestStepDef {
         System.out.println("User launched app: " + app + " with url: " + url + " successfully");
     }
 
-    @When("^User clicks on login button$")
-    public void clickLogin() {
+    @When("^User clicks on login button on (.*)$")
+    public void clickLogin(String app) {
+        if (app.equalsIgnoreCase("flipkart")) {
+            page.locator("(//a[@href='/account/login?ret=/'])[1]").click();
+        }
+        if (app.equalsIgnoreCase("amazon")) {
+            page.getByText("Hello, sign in").click();
+        }
         System.out.println("User clicked on login button successfully");
     }
 
-    @Then("^User is logged in$")
-    public void userLoggedIn() {
+    @Then("^User is logged in (.*)$")
+    public void userLoggedIn(String app) {
+        if (app.equalsIgnoreCase("flipkart")) {
+            PlaywrightAssertions.assertThat(page.getByText("Request OTP")).isVisible();
+        }
+        if (app.equalsIgnoreCase("amazon")) {
+            PlaywrightAssertions.assertThat(page.locator("#continue")).isVisible();
+        }
         System.out.println("User is logged in successfully");
     }
 }
--- src/test/resources/features/PlaywrightTest.feature
+++ src/test/resources/features/PlaywrightTest.feature
@@ -1,11 +1,12 @@
 Feature: This feature file implements and tests integration of playwright and cucumber
 
+  @CucumberTest
   Scenario Outline: Test playwright and cucumber integration
     Given User launches <app> with <url>
-    When User clicks on login button
-    Then User is logged in
+    When User clicks on login button on <app>
+    Then User is logged in <app>
 
     Examples:
       | app      | url                      |
       | flipkart | https://www.flipkart.com |
-      | amazon   | https://www.amazon.com   |
+      | amazon   | https://www.amazon.in    |
