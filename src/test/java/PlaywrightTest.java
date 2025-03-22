1e2f8d42c9b30727641110668f0416a587deba33
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2025-03-21 22:25:59 -0400

    adding playwright.yml file and a test class

---
+++ src/test/java/PlaywrightTest.java
@@ -0,0 +1,41 @@
+import com.microsoft.playwright.APIRequest;
+import com.microsoft.playwright.APIRequestContext;
+import com.microsoft.playwright.APIResponse;
+import com.microsoft.playwright.Playwright;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlaywrightTest {
+
+    Playwright playwright;
+    APIRequestContext apiRequestContext;
+    APIResponse apiResponse;
+    APIRequest.NewContextOptions newContextOptions;
+
+    @BeforeTest
+    public void setContext() {
+        playwright = Playwright.create();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("content-type", "application/json");
+        newContextOptions = new APIRequest.NewContextOptions();
+        newContextOptions.setExtraHTTPHeaders(headers);
+        newContextOptions.setIgnoreHTTPSErrors(true);
+        newContextOptions.setBaseURL("https://fakerapi.it/api/v2/addresses");
+        apiRequestContext = playwright.request().newContext(newContextOptions);
+    }
+
+    @Test
+    public void executeAddressApi() {
+        apiResponse = apiRequestContext.get("");
+        System.out.println(apiResponse.text());
+    }
+
+    @AfterTest
+    public void tearContext() {
+        playwright.close();
+    }
+}
