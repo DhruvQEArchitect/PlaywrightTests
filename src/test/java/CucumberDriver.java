51f668e6fbcb9c967de175f150acee80217f1777
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2025-03-29 23:41:16 -0400

    updating cucumber driver

--- src/test/java/CucumberDriver.java
+++ src/test/java/CucumberDriver.java
@@ -3,7 +3,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(dryRun = true,
+@CucumberOptions(
         monochrome = true,
         features = "src/test/java/features/",
         tags = "@CucumberTest",
